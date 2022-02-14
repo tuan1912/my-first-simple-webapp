8897f3c272c7a4a7a2f077928b0e364577fa6bcb
tuan1912 <nguyentuanktcdt3.hust@gmail.com>
2022-02-14 15:06:23 +0700

    start

---
+++ src/test/java/tuan/nguyen/casemodule4/CaseModule4ApplicationTests.java
@@ -0,0 +1,13 @@
+package tuan.nguyen.casemodule4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaseModule4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
